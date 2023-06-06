4330910c11a565853d5a7edfadce5f5f0718c985
Manjurul Islam <mijony29@gmail.com>
2023-06-06 10:59:13 +0600

    application view open

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -1,13 +1,13 @@
 @extends('frontend.layouts.front-app')
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <div class="jumbotron">
-                <h1 class="text-center text-danger text-uppercase pt-lg-5">
-                    Application No longer Available
-                </h1>
-            </div>
-        </div>
-    </div>
-    {{--    <livewire:student-application/>--}}
+    {{--    <div class="card">--}}
+    {{--        <div class="card-body">--}}
+    {{--            <div class="jumbotron">--}}
+    {{--                <h1 class="text-center text-danger text-uppercase pt-lg-5">--}}
+    {{--                    Application No longer Available--}}
+    {{--                </h1>--}}
+    {{--            </div>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
+    <livewire:student-application/>
 @endsection
